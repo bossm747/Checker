8c9841afef9da59d79e0745e71b65f7c2de630d4
bossm747 <142097219+bossm747@users.noreply.github.com>
2025-05-08 06:38:56 +0000

    ok

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('checkCommission').addEventListener('click', function() {
+    document.getElementById('result').innerText = 'Commission checking is not yet implemented.';
+});
